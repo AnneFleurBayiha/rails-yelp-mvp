9d3cf56696d90ed71600c0a4cfd7af4c812d6811
Anne Fleur Bayiha <anne.bayiha@yahoo.com>
2022-11-23 16:42:03 -0500

    completed app

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # actions: index, show, new, create, edit, update, destroy
   resources :restaurants do
     # actions for review: new
-    resources :reviews, only: [:new]
+    resources :reviews, only: [:new,:create]
   end
 
 end
